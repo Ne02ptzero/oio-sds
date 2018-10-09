5afa5851c672e7e0eab437808f0b8838ef9b9195
Louis Solofrizzo <lsolofrizzo@online.net>
2018-10-09 17:18:33 +0200

    .

--- oio/cli/cluster/cluster.py
+++ oio/cli/cluster/cluster.py
@@ -80,8 +80,7 @@ class ClusterList(lister.Lister):
                 addr = srv['addr']
                 up = tags.get('tag.up', 'n/a')
 
-                print(tags)
-                if srv_type == 'rawx' and tags.get('tag.ia', False) == True:
+                if srv_type == 'rawx' and tags['ia'] == True:
                     srv_type = 'rawx-ia'
 
                 score = srv['score']
