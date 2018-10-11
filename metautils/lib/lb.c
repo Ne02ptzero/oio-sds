afbd3630047b508eac59f93d5fa2334e090ac9f3
Louis Solofrizzo <lsolofrizzo@online.net>
2018-10-11 04:05:04 +0200

    .

--- metautils/lib/lb.c
+++ metautils/lib/lb.c
@@ -66,7 +66,11 @@ oio_lb_world__feed_service_info_list(struct oio_lb_world_s *lbw,
 		}
 
 		/* Insert the service in the main slot */
-		g_snprintf(slot_name, sizeof(slot_name), "%s", srv->type);
+                if (service_info_get_tag(srv, "ia") != NULL)
+		    g_snprintf(slot_name, sizeof(slot_name), "%s-ia", srv->type);
+                else
+		    g_snprintf(slot_name, sizeof(slot_name), "%s", srv->type);
+
                 GRID_DEBUG("Slot_name = %s", slot_name);
 		oio_lb_world__create_slot(lbw, slot_name);
 		oio_lb_world__feed_slot(lbw, slot_name, item);
